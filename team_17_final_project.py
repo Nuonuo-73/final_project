f34ad8002951f83725c5b3eae8c70aa3fe8b044d
Nuonuo <LIjiayi_@smail.swufe.edu.cn>
2022-11-04 14:15:46 +0800

    new update

--- team_17_final_project.py
+++ team_17_final_project.py
@@ -11,20 +11,14 @@ import json
 import cufflinks as cf
 cf.set_config_file(offline=True)
 
+st.title('Best Cities & Countries for Startups')
 
 plt.style.use('seaborn')
 
-# show the title
-st.title('Best Cities & Countries for Startups')
-
-# read csv and show the dataframe
-df = pd.read_csv('best cities for startups in 2022 - in 2022.csv')
-
+df = pd.read_csv('best cities for startups in 2022 - in 2022.csv', encoding='gbk')
 df_2 = df['city'].str.split(',', expand = True)
 df_3 = df.drop('city', axis = 1).join(df_2)
-df_3.rename(columns={"quantity score\n":'quantity score', 'quality score\r\n':'quality score','business score\r\n':'business score','sign of change in position\r\n':'sign of change in position'},inplace=True)
 df_3.rename(columns={0:'city', 1:'country'}, inplace=True)
-
 df_3.index = np.arange(1, len(df)+1)
 st.dataframe(df_3)
 
@@ -32,15 +26,11 @@ df_country = df_3.groupby(['country']).mean()
 df_country.sort_values(['total score'], axis=0, ascending=False, inplace=True)
 df_country = df_country.reset_index()
 
-#according to the total score, rank and draw a and histogram of top 10 countries for startups
-##fig, ax1 = plt.subplots(figsize=(15,8))
-#df_country.head(10)['total score'].plot.bar().set_xticks(df_country.index[0:10], df_country.country[0:10], rotation=60)
-#ax1.set_title('Top 10 Countries for Startups\n(Total Score)')
+#图1柱状图
 df_4 = df_country.head(10)
 df_5 = df_4[['country','total score']].set_index('country')
 fig_1 = df_5.iplot(asFigure=True, kind='bar',title='Top 10 Countries for Startups\n(Total Score)')
 st.plotly_chart(fig_1)
-
 
 #add a new column to the existing interactive table
 city_count_list = []
@@ -52,24 +42,19 @@ for country in df_country.country:
 df_city_count = df_3.country
 result_city_count = df_city_count.value_counts()
 
-#fig, ax2 = plt.subplots(figsize = (15,8))
+#图2柱状图
 fig_2 = result_city_count[::-1][-10:].iplot(asFigure=True, kind='barh',title='Top 10 Countries for Startups\n(Best City Total)')
-#ax2.set_title('Top 10 Countries for Startups\n(Best City Total)')
-#ax2.set_xlabel('number of best cities')
 st.plotly_chart(fig_2)
-
 
 #show new interactive table(score_top_10)
 df_country.index = np.arange(1, len(df_country)+1)
 df_country = df_country.drop('position', axis = 1)
 df_country['best cities'] = city_count_list
-df_country[:10]
+st.dataframe(df_country[:10])
 
-#pie(proportion of the best cities in the top ten countries)
-#To make the pie chart more beautiful, the tags do not overlap, and the list is directly set.
-#The following is the original code
+#为饼图添加标题
 st.subheader('   Proportion of the Best Cities in the Top 10 Countries')
-
+#制作饼图
 score_top_countries = df_country[:10].country.tolist()#[' United Arab Emirates', ' Singapore', ' China', ' United States', ' South Korea', ' Israel', ' Estonia', ' Indonesia', ' Japan', ' India']#
 value = df_country[:10]['best cities'].tolist()#[3, 1, 44, 257, 5, 13, 2, 5, 11, 37]#
 
@@ -103,25 +88,23 @@ streamlit_echarts.st_pyecharts(
     )
 
 
-
-# analyze the impact of each kind of scores on entrepreneurship by using stack graph
+#制作堆叠图
 top_cities = df.head(20)
-top_cities_info = top_cities[['city', 'quantity score\\n', 'quality score\\r\\n', r'business score\\r\\n']].set_index('city')
-##fig, ax3 = plt.subplots(figsize= (15,5))
+top_cities_info = top_cities[['city', 'quantity score', 'quality score', 'business score']].set_index('city')
+
 fig_3 = top_cities_info.iplot(asFigure=True, kind='bar',barmode='stack',title='Inpact of different scores')
 st.plotly_chart(fig_3)
 
-#line chart
-fig, ax4 = plt.subplots(1, 3, figsize=(15,5),facecolor="#EFE9E6")
-ax4[0].plot(top_cities[['total score','quality score\r\n']], marker='o')
-ax4[1].plot(top_cities[['total score','quantity score\n']], marker='o')
-ax4[2].plot(top_cities[['total score','business score\r\n']], marker='o')
+#制作折线图
+fig, ax4 = plt.subplots(1, 3, figsize=(15,5))
+ax4[0].plot(top_cities[['total score','quality score']], marker='o')
+ax4[1].plot(top_cities[['total score','quantity score']], marker='o')
+ax4[2].plot(top_cities[['total score','business score']], marker='o')
 st.pyplot(fig, ax4[0])
 st.pyplot(fig, ax4[1])
 st.pyplot(fig, ax4[2])
 
-
-#城市在中国的分布的地图
+#城市在中国的分布地的数据获取
 st.subheader('   Distribution of the best cities in China')
 ##获取城市的地理信息
 for i in df_3[df_3['country'] == ' China']['city'].index:
@@ -133,7 +116,7 @@ addr_city = df_3[df_3['country'] == ' China']['city'].tolist()
 def gaode(addr_city):
     para = {
         "address": quote(addr_city), #传入地址参数
-        "key": "6c4d227098e7cda4345c79be1778faa6" #百度地图开放平台申请ak
+        "key": "6c4d227098e7cda4345c79be1778faa6" #高德地图开放平台申请ak
     }
     url = "https://restapi.amap.com/v3/geocode/geo?"
     req = requests.get(url,para)
@@ -153,6 +136,8 @@ df_china.index = np.arange(1, len(df_china)+1)
 df_china['lat'] = lat_list
 df_china['lon'] = lon_list
 df_china = df_china.drop('country', axis=1)
+
+#侧边栏筛选项
 #创建新列
 position_change = []
 
@@ -182,9 +167,10 @@ st.dataframe(df_china)
 
 ##设立侧边栏筛选项
 ###输入值作为筛选
-form = st.sidebar.form("total_score_form")
+form = st.sidebar.form('total_score_form')
 total_score_filter = form.text_input('The total score is above?(please enter a integer number)', '0')
-form.form_submit_button("Apply")
+form.form_submit_button('Apply')
+
 ###变动作为筛选
 position_change_filter = st.sidebar.multiselect(
     'World ranking compared with 2021',
@@ -237,6 +223,4 @@ elif business_score_filter == 'higher than 5':
 df_china['lon'] = df_china['lon'].astype(float)
 df_china['lat'] = df_china['lat'].astype(float)
 
-#df_china.to_csv('map-city-china.csv', index= False)
-#df_map = pd.read_csv('map-city-china.csv')
 st.map(df_china)
