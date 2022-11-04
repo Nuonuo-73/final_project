3e947e7813bf95f3451216a03e0f6fd40109f9cc
Nuonuo <LIjiayi_@smail.swufe.edu.cn>
2022-11-04 18:19:12 +0800

    last update(maybe)

--- team_17_final_project.py
+++ team_17_final_project.py
@@ -1,17 +1,28 @@
 from urllib.request import urlopen, quote
 from pyecharts.charts import Pie
 from pyecharts import options as opts
+from PIL import Image
+from plotly.subplots import make_subplots
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 import streamlit as st
 import streamlit_echarts
+import plotly.graph_objects as go
 import requests
 import json
 import cufflinks as cf
+
 cf.set_config_file(offline=True)
 
+image = Image.open('picture.png')
+st.image(image, use_column_width=True)
+
 st.title('Best Cities & Countries for Startups')
+st.text('The dataset has about 1000 unique values and 9 columns. It shows best startups from\ndifferent cities and countries. Each startup is matched with four scores showing its\ncapacity in different aspects. Doing research on this dataset will provide us with\nknowledge about how to manage a success startup.')
+st.header('Revised DataFrame') ###
+st.text('We split the \'city \' column into two columns \'city\'  \'country\' to make further \nresearch easier.')
 
 plt.style.use('seaborn')
 
@@ -27,10 +38,14 @@ df_country.sort_values(['total score'], axis=0, ascending=False, inplace=True)
 df_country = df_country.reset_index()
 
 #图1柱状图
+st.header('Analysis 1: Best countries for startups')
+st.subheader('Histogram of top 10 countries for startups') ###
+st.text('The histogram shows top 10 cities with highest total score.')
 df_4 = df_country.head(10)
 df_5 = df_4[['country','total score']].set_index('country')
 fig_1 = df_5.iplot(asFigure=True, kind='bar',title='Top 10 Countries for Startups\n(Total Score)')
 st.plotly_chart(fig_1)
+st.text('We can find that Singapore has the highest average score for the best cities.')
 
 #add a new column to the existing interactive table
 city_count_list = []
@@ -43,8 +58,10 @@ df_city_count = df_3.country
 result_city_count = df_city_count.value_counts()
 
 #图2柱状图
+st.subheader('Number of best cities in a country')
 fig_2 = result_city_count[::-1][-10:].iplot(asFigure=True, kind='barh',title='Top 10 Countries for Startups\n(Best City Total)')
 st.plotly_chart(fig_2)
+st.text(' Although Singapore has the highest average score for the best cities, its number of\nbest cities is not even in the top ten. The US has the highest number of best cities\nand a high average score.')
 
 #show new interactive table(score_top_10)
 df_country.index = np.arange(1, len(df_country)+1)
@@ -53,7 +70,8 @@ df_country['best cities'] = city_count_list
 st.dataframe(df_country[:10])
 
 #为饼图添加标题
-st.subheader('   Proportion of the Best Cities in the Top 10 Countries')
+st.subheader('Proportion of the Best Cities in the Top 10 Countries')
+st.text('It is an interactive pie. By clicking taggings on the right, you can add or erase\ncountries from pie chart.')
 #制作饼图
 score_top_countries = df_country[:10].country.tolist()#[' United Arab Emirates', ' Singapore', ' China', ' United States', ' South Korea', ' Israel', ' Estonia', ' Indonesia', ' Japan', ' India']#
 value = df_country[:10]['best cities'].tolist()#[3, 1, 44, 257, 5, 13, 2, 5, 11, 37]#
@@ -87,23 +105,59 @@ streamlit_echarts.st_pyecharts(
         )
     )
 
+st.text('By looking at the pie chart, we can draw the conclusion that America is indeed the\nbest country for startups.')
+
+# analyze the impact of each kind of scores on entrepreneurship by using stack graph
+st.header('Analysis 2: Impact of each kind of scores on startups') ###
+st.subheader('Stacked chart of different scores')
+st.text('In order to find clues about what element can affect a startup most, we use stack\nchart to analyze.')
 
 #制作堆叠图
 top_cities = df.head(20)
 top_cities_info = top_cities[['city', 'quantity score', 'quality score', 'business score']].set_index('city')
 
-fig_3 = top_cities_info.iplot(asFigure=True, kind='bar',barmode='stack',title='Inpact of different scores')
+fig_3 = top_cities_info.iplot(asFigure=True, kind='bar',barmode='stack')
 st.plotly_chart(fig_3)
+st.text('It is obvious that quality score accounts for the most part of total scores. We can\nassume that quality scores are determinant of total scores. To test the hypothesis,\nwe can use the line chart.')
 
 #制作折线图
-fig, ax4 = plt.subplots(1, 3, figsize=(15,5))
-ax4[0].plot(top_cities[['total score','quality score']], marker='o')
-ax4[1].plot(top_cities[['total score','quantity score']], marker='o')
-ax4[2].plot(top_cities[['total score','business score']], marker='o')
-st.pyplot(fig, ax4[0])
-st.pyplot(fig, ax4[1])
-st.pyplot(fig, ax4[2])
+st.subheader('Relationship between total score and other scores') ###
+fig_4 = make_subplots(rows=2, cols=2,subplot_titles=['Total & Other Scores','Total & Quality Scores','Total & Quantity Scores','Total & Business Scores'])
+x_list = [i + 1 for i in range(20)]
 
+fig_4.add_trace(go.Scatter(x=x_list, y=top_cities['total score'].to_list(), name='total score'), 
+              row=1, col=1)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['quality score'].to_list(), name='quality score'), 
+              row=1, col=1)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['quantity score'].to_list(), name='quantity score'), 
+              row=1, col=1)
+fig_4.add_trace(go.Scatter(y=top_cities['business score'].to_list(), name='business score'), 
+              row=1, col=1)
+fig_4.add_trace(go.Scatter(x=x_list, y=top_cities['total score'].to_list(), name='total score'), 
+              row=1, col=2)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['quality score'].to_list(), name='quality score'), 
+              row=1, col=2)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['total score'].to_list(), name='total score'), 
+              row=2, col=1)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['quantity score'].to_list(), name='quantity score'), 
+              row=2, col=1)
+fig_4.add_trace(go.Scatter(x=x_list,y=top_cities['total score'].to_list(), name='total score'), 
+              row=2, col=2)
+fig_4.add_trace(go.Scatter(y=top_cities['business score'].to_list(), name='business score'), 
+              row=2, col=2)
+
+fig_4.update_xaxes(title_text="countries' position", row=1, col=1) 
+fig_4.update_xaxes(title_text="countries' position", row=1, col=2) 
+fig_4.update_xaxes(title_text="countries' position", row=2, col=1)
+fig_4.update_xaxes(title_text="countries' position", row=2, col=2)  
+
+fig_4.update_yaxes(title_text="score", row=1, col=1)
+fig_4.update_yaxes(title_text="score", row=2, col=1)
+
+fig_4.update_layout(height=500, width = 700)
+st.plotly_chart(fig_4)
+st.text('We can see that the line of quality and the line of total score is quite close to\neach other and their trends are most the same. However, when it comes to quantity\nline and bussiness line, they are almost flat and have little relation with total\nscores.')
+st.text('So quality score, that is, environmental score, plays the most important role in\naffecting the total score.')
 
 ##获取城市的地理信息
 #for i in df_3[df_3['country'] == ' China']['city'].index:
@@ -186,15 +240,15 @@ position_change_filter = st.sidebar.multiselect(
 ###选择作为筛选
 business_score_filter = st.sidebar.radio(
     "Choose business score level",
-    ('higher than 2', 'higher than 1', 'all')
+    ('>2','>1','all')
 )
 quality_score_filter = st.sidebar.radio(
     "Choose quality level",
-    ('higher than 50', 'higher than 10', 'all')
+    ('>50','>10','all')
 )
 quantity_score_filter = st.sidebar.radio(
     "Choose quantity level",
-    ('higher than 5', 'higher than 1', 'all')
+    ('>5','>1','all')
 )
 
 ###filter by these filter
@@ -203,29 +257,34 @@ if total_score_filter!= '0':
 #这次应该正确
 df_map = df_map[df_map['position change'].isin(position_change_filter)]
 
-
+if business_score_filter == '>2':
+    df_map = df_map[df_map['business score'] > 2]
+if business_score_filter == '>1':
+    df_map = df_map[df_map['business score'] > 1]
 if business_score_filter == 'all':
-    df_map = df_map[df_map['business score'].astype(float) > 0]
-elif business_score_filter == 'higher than 1':
-    df_map = df_map[df_map['business score'].astype(float) > 1]
-elif business_score_filter == 'higher than 2':
-    df_map = df_map[df_map['business score'].astype(float) > 2]
+    df_map = df_map[df_map['business score'] > 0]
 
+if quality_score_filter == '>50':
+    df_map = df_map[df_map['quality score'].astype(float) > 50]
+if quality_score_filter == '>10':
+    df_map = df_map[df_map['quality score'].astype(float) > 10]
 if quality_score_filter == 'all':
     df_map = df_map[df_map['quality score'].astype(float) > 0]
-elif business_score_filter == 'higher than 10':
-    df_map = df_map[df_map['quality score'].astype(float) > 10]
-elif business_score_filter == 'higher than 50':
-    df_map = df_map[df_map['quality score'].astype(float) > 50]
 
+
+if quantity_score_filter == '>5':
+    df_map = df_map[df_map['quantity score'].astype(float) > 5]
+if quantity_score_filter == '>1':
+    df_map = df_map[df_map['quantity score'].astype(float) > 1]
 if quantity_score_filter == 'all':
     df_map = df_map[df_map['quantity score'].astype(float) > 0]
-elif business_score_filter == 'higher than 1':
-    df_map = df_map[df_map['quantity score'].astype(float) > 1]
-elif business_score_filter == 'higher than 5':
-    df_map = df_map[df_map['quantity score'].astype(float) > 5]
+
+
+
 
 ##绘制地图
-
-
+st.text('It is an interactive map.')
+st.text('we use the geocoding of Gaode Mapsthe to determine the latitude and longitude of\ncities in China for our map.')
+st.text('The map can show best cities for starups in China.')
+st.text('* You can choose different conditions on the sidebar.')
 st.map(df_map)
